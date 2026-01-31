8c95f9e5fd25a878f46871fb5c330f6a8b26d91f
Jake Dickinson <Jakedickinson@u.boisestate.edu>
2026-01-30 23:27:12 -0700

    Updated App.vue and added a BSU logo img

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+ const handleClick = () => {
+    ;
+ };
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <img src="/BroncoLogo.svg" class="logo" alt="Vite logo" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <div>
+    <input v-model="username" placeholder="Enter User ID" />
+  </div>
+  <div> 
+    <button @click="handleClick">Login</button>
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
