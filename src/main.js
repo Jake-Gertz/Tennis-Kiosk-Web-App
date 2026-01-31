3f6e342fc5a7bc26dcf61176ff17ea2fbf852072
Jake Dickinson <Jakedickinson@u.boisestate.edu>
2026-01-31 11:17:33 -0700

    Landing page first draft set up

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import LandingPage from './pages/LandingPage.vue'
 
-createApp(App).mount('#app')
+createApp(LandingPage).mount('#LandingPage')
---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+
+const username = ref('');
+
+ const handleClick = () => {
+    ;
+ };
+</script>
+
+<template>
+  <div class ="landingPageContainer">
+    <div class = "loginSlice">
+        <img src="/BroncoLogo.svg" class="logo" alt="Bronco Logo" />
+      
+      <div class="loginContainer">
+        <input v-model="username" placeholder="Enter User ID" />
+        <button @click="handleClick">Login</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  
+</style>
